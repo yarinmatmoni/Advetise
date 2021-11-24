a3d6b89c4d201827c3d27f09683f8f70661148b7
LilachBachar <Lilachbac@gmail.com>
2021-11-24 11:59:19 +0200

    Interval fix

--- work.js
+++ work.js
@@ -1,10 +1,20 @@
 $.getJSON( "./data.json", function( data ) {
 
+
     $(document).ready(function(){
       let size = data.data.length;
-      let i=1;
+      let i=0;
       firstAdv();
-      setInterval(function(){
+     
+      let firstTime = data.data[0].duration*10000;
+      var run = setInterval(request , firstTime); 
+ 
+     // need to find how we fix the first interval twice.
+
+      function request(){
+
+        console.log(firstTime);
+
         $("body").css("background",data.data[i].colors.background);
         $(".line1").text(data.data[i].text.line1)
           .css("color",data.data[i].colors.line1color);
@@ -15,16 +25,36 @@ $.getJSON( "./data.json", function( data ) {
         $(".line4").text(data.data[i].text.line4)
           .css("color",data.data[i].colors.line4color);
           $("img").attr("src",data.data[i].imgsrc);
-          if(i==size-1)
-            i=0;
-          else
-            i++;
-      },3000);
+       
+       
+        
+        clearInterval(run); 
+
+
+        firstTime = data.data[i].duration*10000;
+
+        if(i==size-1)
+          i=0;
+        else
+          i++;    
+
+        run = setInterval(request, firstTime);
+
+
+      }
+
     });
+
+
 });
+
+
+
+
 //eden
 function firstAdv(){
   $.getJSON( "./data.json", function( data ){
+    console.log("first ad");
     $("body").css("background",data.data[0].colors.background);
     $(".line1").text(data.data[0].text.line1)
       .css("color",data.data[0].colors.line1color);
