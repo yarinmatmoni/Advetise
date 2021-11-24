a64ef1592157ccd3b12234ff958e8c1aaf7d162b
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-11-24 09:34:19 +0200

    yarin cheack

--- work.js
+++ work.js
@@ -2,16 +2,38 @@ $.getJSON( "./data.json", function( data ) {
 
     $(document).ready(function(){
       let size = data.data.length;
-      console.log(data.data[0].colors.line1color);
-      $("body").css("background",data.data[0].colors.background);
-      $(".line1").text(data.data[0].text.line1)
-        .css("color",data.data[0].colors.line1color);
-      $(".line2").text(data.data[0].text.line2)
-        .css("color",data.data[0].colors.line2color);
-      $(".line3").text(data.data[0].text.line3)
-        .css("color",data.data[0].colors.line3color);
-      $("img").attr("src",data.data[0].imgsrc);
+      let i=1;
+      firstAdv();
+      setInterval(function(){
+        $("body").css("background",data.data[i].colors.background);
+        $(".line1").text(data.data[i].text.line1)
+          .css("color",data.data[i].colors.line1color);
+        $(".line2").text(data.data[i].text.line2)
+          .css("color",data.data[i].colors.line2color);
+        $(".line3").text(data.data[i].text.line3)
+          .css("color",data.data[i].colors.line3color);
+        $(".line4").text(data.data[i].text.line4)
+          .css("color",data.data[i].colors.line4color);
+          $("img").attr("src",data.data[i].imgsrc);
+          if(i==size-1)
+            i=0;
+          else
+            i++;
+      },3000);
     });
-    
 });
 
+function firstAdv(){
+  $.getJSON( "./data.json", function( data ){
+    $("body").css("background",data.data[0].colors.background);
+    $(".line1").text(data.data[0].text.line1)
+      .css("color",data.data[0].colors.line1color);
+    $(".line2").text(data.data[0].text.line2)
+      .css("color",data.data[0].colors.line2color);
+    $(".line3").text(data.data[0].text.line3)
+      .css("color",data.data[0].colors.line3color);
+    $(".line4").text(data.data[0].text.line4)
+      .css("color",data.data[0].colors.line4color);
+      $("img").attr("src",data.data[0].imgsrc);
+  });
+}
