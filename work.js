29d1f02749b822e5556316fd1e4162e671619bd5
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-11-26 01:53:10 +0200

    change css + solve the problem of dileay in first time

--- work.js
+++ work.js
@@ -4,10 +4,10 @@ $.getJSON( "./data.json", function( data ) {
     $(document).ready(function(){
       let size = data.data.length;
       let i=0;
-      firstAdv();
+      //firstAdv();
      
       let firstTime = data.data[0].duration*10000;
-      var run = setInterval(request , firstTime); 
+      var run = setInterval(function(){ request() , firstTime}); //solve problem - first time twice
  
      // need to find how we fix the first interval twice.
 
@@ -24,13 +24,9 @@ $.getJSON( "./data.json", function( data ) {
           .css("color",data.data[i].colors.line3color);
         $(".line4").text(data.data[i].text.line4)
           .css("color",data.data[i].colors.line4color);
-          $("img").attr("src",data.data[i].imgsrc);
+        $("img").attr("src",data.data[i].imgsrc);
        
-       
-        
         clearInterval(run); 
-
-
         firstTime = data.data[i].duration*10000;
 
         if(i==size-1)
@@ -39,8 +35,6 @@ $.getJSON( "./data.json", function( data ) {
           i++;    
 
         run = setInterval(request, firstTime);
-
-
       }
 
     });
@@ -51,19 +45,19 @@ $.getJSON( "./data.json", function( data ) {
 
 
 
-//eden
-function firstAdv(){
-  $.getJSON( "./data.json", function( data ){
-    console.log("first ad");
-    $("body").css("background",data.data[0].colors.background);
-    $(".line1").text(data.data[0].text.line1)
-      .css("color",data.data[0].colors.line1color);
-    $(".line2").text(data.data[0].text.line2)
-      .css("color",data.data[0].colors.line2color);
-    $(".line3").text(data.data[0].text.line3)
-      .css("color",data.data[0].colors.line3color);
-    $(".line4").text(data.data[0].text.line4)
-      .css("color",data.data[0].colors.line4color);
-      $("img").attr("src",data.data[0].imgsrc);
-  });
-}
+// //eden
+// function firstAdv(){
+//   $.getJSON( "./data.json", function( data ){
+//     console.log("first ad");
+//     $("body").css("background",data.data[0].colors.background);
+//     $(".line1").text(data.data[0].text.line1)
+//       .css("color",data.data[0].colors.line1color);
+//     $(".line2").text(data.data[0].text.line2)
+//       .css("color",data.data[0].colors.line2color);
+//     $(".line3").text(data.data[0].text.line3)
+//       .css("color",data.data[0].colors.line3color);
+//     $(".line4").text(data.data[0].text.line4)
+//       .css("color",data.data[0].colors.line4color);
+//       $("img").attr("src",data.data[0].imgsrc);
+//   });
+// }
