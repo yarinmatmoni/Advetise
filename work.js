57abdd5cf39794b30ee3906f4700a916c6ca3e1f
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-11-26 14:09:50 +0200

    remove comments

--- work.js
+++ work.js
@@ -1,20 +1,16 @@
 $.getJSON( "./data.json", function( data ) {
-
-
+ 
     $(document).ready(function(){
       let size = data.data.length;
       let i=0;
-      //firstAdv();
-     
       let firstTime = data.data[0].duration*10000;
       var run = setInterval(function(){ request() , firstTime}); //solve problem - first time twice
  
-     // need to find how we fix the first interval twice.
-
       function request(){
 
         console.log(firstTime);
 
+        $("img").attr("src",data.data[i].imgsrc);
         $("body").css("background",data.data[i].colors.background);
         $(".line1").text(data.data[i].text.line1)
           .css("color",data.data[i].colors.line1color);
@@ -24,40 +20,18 @@ $.getJSON( "./data.json", function( data ) {
           .css("color",data.data[i].colors.line3color);
         $(".line4").text(data.data[i].text.line4)
           .css("color",data.data[i].colors.line4color);
-        $("img").attr("src",data.data[i].imgsrc);
-       
+                  
         clearInterval(run); 
         firstTime = data.data[i].duration*10000;
 
-        if(i==size-1)
+       if(i==size-1)
           i=0;
-        else
+       else
           i++;    
 
         run = setInterval(request, firstTime);
       }
 
     });
-
-
+    
 });
-
-
-
-
-
-// function firstAdv(){
-//   $.getJSON( "./data.json", function( data ){
-//     console.log("first ad");
-//     $("body").css("background",data.data[0].colors.background);
-//     $(".line1").text(data.data[0].text.line1)
-//       .css("color",data.data[0].colors.line1color);
-//     $(".line2").text(data.data[0].text.line2)
-//       .css("color",data.data[0].colors.line2color);
-//     $(".line3").text(data.data[0].text.line3)
-//       .css("color",data.data[0].colors.line3color);
-//     $(".line4").text(data.data[0].text.line4)
-//       .css("color",data.data[0].colors.line4color);
-//       $("img").attr("src",data.data[0].imgsrc);
-//   });
-// }
