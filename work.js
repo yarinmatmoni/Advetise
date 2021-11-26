495724c0e69945db2509e9da7177b0c120bc3c01
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-11-26 23:31:25 +0200

    Reorganization of the project

--- work.js
+++ work.js
@@ -4,34 +4,34 @@ $.getJSON( "./data.json", function( data ) {
       let size = data.data.length;
       let i=0;
       let firstTime = data.data[0].duration*10000;
-      var run = setInterval(function(){ request() , firstTime}); //solve problem - first time twice
- 
+      var run = setInterval(function(){ request() , firstTime});
+      
       function request(){
 
-        console.log(firstTime);
-        if(data.data[i].imgsrc != null){
-          $("img").attr("src",data.data[i].imgsrc);
-          $("body").css("background",data.data[i].colors.background);
-          $(".line1").text(data.data[i].text.line1)
-            .css("color",data.data[i].colors.line1color);
-          $(".line2").text(data.data[i].text.line2)
-            .css("color",data.data[i].colors.line2color);
-          $(".line3").text(data.data[i].text.line3)
-            .css("color",data.data[i].colors.line3color);
-          $(".line4").text(data.data[i].text.line4)
-            .css("color",data.data[i].colors.line4color);
-        }
+        displayAdv(i)         
         clearInterval(run); 
         firstTime = data.data[i].duration*10000;
-
-       if(i==size-1)
+        if(i==size-1)
           i=0;
-       else
+        else
           i++;    
 
         run = setInterval(request, firstTime);
       }
-
+      
     });
+
+    function displayAdv(i){
+      $("img").attr("src",data.data[i].imgsrc);
+      $("body").css("background",data.data[i].colors.background);
+      $(".line1").text(data.data[i].text.line1)
+        .css("color",data.data[i].colors.line1color);
+      $(".line2").text(data.data[i].text.line2)
+        .css("color",data.data[i].colors.line2color);
+      $(".line3").text(data.data[i].text.line3)
+        .css("color",data.data[i].colors.line3color);
+      $(".line4").text(data.data[i].text.line4)
+        .css("color",data.data[i].colors.line4color);
+    }
     
 });
