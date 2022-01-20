207233c99d50e2f27a1801491c09e5055da81095
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2022-01-20 14:42:46 +0200

    yarin

--- server.js
+++ server.js
@@ -92,8 +92,9 @@ app.get("/", function (request, response) {
                     if (err) throw err;
                     if(result[0].userName == username && result[0].password == userPassword){
                       console.log("ok");
-                      // connect to dashboard
-
+                      // app.get('/login', function (req, res) {
+                      //   res.sendFile(__dirname + "/dashboard.html");
+                      // });                    
                     }else{
                       socket.emit("validition");
                     }
@@ -104,11 +105,6 @@ app.get("/", function (request, response) {
           break;
         }
   }
-});
-
-// ptoblem - everyone have acssess
-app.get('/login', function (req, res) {
-  res.sendFile(__dirname + "/dashboard.html");
 });
 
 server.listen(8080);
